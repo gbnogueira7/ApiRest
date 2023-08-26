b94f693923f5979d0f39c45f724526bc589e3d45
gnogueira <gabrielnogueiraramos@gmail.com>
2023-08-26 11:42:07 -0300

    Adição do método put e método delete

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json());
 
 const livros =[
-    {id: 1, "título": "Senhor dos anéis"},
-    {id: 2, "título": "O hobbit"},
+    {id: 1, "titulo": "Senhor dos anéis"},
+    {id: 2, "titulo": "O hobbit"},
 ]
 
 app.get('/', (req, res) => {
@@ -22,5 +22,29 @@ app.post('/livros', (req, res) => {
     res.status(201).send('O livro foi cadastrado com sucesso')
 })
 
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+function buscaLivro(id){
+return livros.findIndex(livro => livro.id == id)
+//FindIndex, método para manipular array, que acha, através de um
+//parâmetro a posição onde está o elemento que você deseja alterar
+}
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id); 
+    livros.splice(index, 1);
+    //Posição de início, e qual posição queremos excluir
+    res.send(`O livro ${id} foi deletado com sucesso`);
+})
 
 export default app
